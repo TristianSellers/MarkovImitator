a7b5567649ea3d7d4087c6bdd0c6f9b91db7f06d
Tristian Sellers <tsellers@xula.edu>
2023-09-22 14:19:57 +0000

    Initialized Chain Object file

--- Imitator.java
+++ Imitator.java
@@ -1,6 +1,13 @@
 public class Imitator {
     public static void main(String [] args) {
-        
+        /*
+        Implement the algorithm as a program that accepts a 
+        book-length input given as a command line
+        argument. Produces an imitation of 100-200 words. 
+        Use prefixes of 2 or 3 words. Upload your solution
+        to GitHub as a private repository, to which you add 
+        raylangnola as a collaborator        
+        */
     }
     /// function(s)
 }
