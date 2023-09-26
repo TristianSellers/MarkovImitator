25cf08a6ad2f69e76a7f78e048b70ca7b3118815
Tristian Sellers <tsellers@xula.edu>
2023-09-26 22:20:23 +0000

    Revisualized Chain & Imitator; Works

--- Chain.java
+++ Chain.java
@@ -1,51 +1,65 @@
 import java.util.*;
 import java.io.*;
 public class Chain {
-    private List<String> pref;
+    private List<String> prefix;
 
     public Chain() {
-        pref = new ArrayList<>();
+        prefix = new ArrayList<>();
     }
 
     public Chain(List<String> list, int n) {
-        pref = new ArrayList<>();
+        prefix = new ArrayList<>();
         for(String x : list) {
-            if(pref.size() == n) {
+            if(prefix.size() == n) {
                 break;
             }
             else {
-                pref.add(x);
+                prefix.add(x);
             }
         }
     }
 
-    public List<String> getPref() {
-        return pref;
+    public List<String> getPrefix() {
+        return prefix;
     }
 
-    public Map<List<String>, List<String>> build(List<String> list) {
+    public Map<List<String>, List<String>> build(List<String> words, int n) {
         Map<List<String>, List<String>> result = new HashMap<>();
-        result.put(pref, new ArrayList<>());
-        for(int i = 0; i < list.size(); i++) {
-            String curr = list.get(i);
-            // System.out.printf("curr after assignment from list.get(%d): %s\n",
-                        // i,curr);
-            if(!pref.contains(curr)){
-                // System.out.println(pref);
-                if(result.get(pref) == null) {
-                    result.put(pref, new ArrayList<>());
+        for(int i = 0; i < words.size(); i++) {
+            // System.out.println(prefix);
+            int ndx = i + n;
+            if(ndx >= words.size()) {
+                break;
+            }
+            else {
+                if(!result.containsKey(prefix)) {
+                    result.put(prefix, new ArrayList<>());
                 }
-                List<String> valueList = result.get(pref);
-                // System.out.println(valueList);
-                valueList.add(curr);
-                // System.out.println(valueList);
-                result.put(pref, valueList);
-                pref.remove(0);
-                pref.add(curr);
+                List<String> currVList = result.get(prefix);
+                currVList.add(words.get(ndx));
+                prefix.add(words.get(ndx));
+                prefix.remove(0);
+                // if(currVList.size() > 1) {
+                //     System.out.println(currVList.size());
+                // }
             }
         }
         return result;
-    } 
+    }
+
+    public List<String> createImitator(Map<List<String>, List<String>> map) {
+        List<String> result = new ArrayList<>();
+        for(String x : prefix) {
+            result.add(x);
+        }
+        for(Map.Entry<List<String>, List<String>> x : map.entrySet()) {
+            List<String> currentList = map.get(prefix);
+            int randomIndex = random.nextInt(currentList.size());
+            
+        }
+        return result;
+        }
+    }
 
     @Override
     public boolean equals (Object o) {
@@ -54,12 +68,12 @@ public class Chain {
             return false;
         }
         Chain c = (Chain)o;
-        return pref == c.getPref();
+        return prefix == c.getPrefix();
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(pref);
+        return Objects.hash(prefix);
     }
 
 }
